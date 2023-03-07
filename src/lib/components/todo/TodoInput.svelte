f70afebda59c37939d127d87c2f7683bf4c3103e
afrotter <jdengelbrecht97@gmail.com>
2023-03-07 15:50:07 +0200

    Added todo input and todo section

--- src/lib/components/todo/TodoInput.svelte
+++ src/lib/components/todo/TodoInput.svelte
@@ -2,14 +2,21 @@
     import { deleteTodo, editTodo, toggleComplete } from "../../store/TodoStore";
 
      export let todo;
+
+     $: completeClass = todo.complete ? 'bg-green-three' : 'bg-leaf-one';
 </script>
 
-<div>
-    <div>
-        <label for={`${todo.id}-checkbox`}>Complete todo</label>
-        <input id={`${todo.id}-checkbox`} type="checkbox" checked={todo.complete} on:change={() => toggleComplete()}/>
+<div class="flex items-center justify-between rounded-md border-2 border-gray-one px-5 py-4">
+    <div class="flex w-full max-w-lg items-center justify-start">
+        <label for={`${todo.id}-checkbox`} class="sr-only">Complete todo</label>
+        <input id={`${todo.id}-checkbox`} 
+		type="checkbox" 
+		checked={todo.complete} 
+		on:change={() => toggleComplete()}
+		class="h-4 w-4 rounded border border-gray-three bg-cream-four text-green-four focus:border-green-five focus:outline focus:outline-2 focus:outline-offset-2 focus:outline-green-five" 
+		/>
     
-        <label for={`${todo.id}-text`}>Edit todo</label>
+        <label for={`${todo.id}-text`} class="sr-only">Edit todo</label>
         <input
 			id={`${todo.id}-text`}
 			type="text"
@@ -17,13 +24,14 @@
 			value={todo.text}
 			on:input={(e) => editTodo(todo.id, e.target.value)}
 			class="ml-5 flex-1 text-ellipsis rounded-none border-x-0 border-t-0 border-b border-dashed border-b-gray-two bg-cream-four px-0 pb-1 text-base font-normal text-gray-three placeholder:text-gray-two focus:border-gray-three focus:outline-none focus:ring-0"
-		/>
-        <span>
+		
+			/>
+        <span class="{completeClass} ml-5 hidden rounded-full py-0.5 px-2 text-sm font-normal text-gray-five md:block">
             {todo.complete ? 'Complete' : 'In Progress'}
         </span>
     </div>
-    <button type="button" on:click={() => deleteTodo(todo.id)}>
-        <span>
+    <button type="button" on:click={() => deleteTodo(todo.id)} class="group ml-4 flex items-center justify-center rounded-md bg-cream-four p-2 hover:bg-steel-one focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-five">
+        <span class="sr-only">
             Delete todo
         </span>
         <svg
